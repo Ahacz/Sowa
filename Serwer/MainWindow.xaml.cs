25bb7977c313a40fa049f52f90697cb2703d6118
Ahacz <37111467+Ahacz@users.noreply.github.com>
2019-12-15 23:44:32 +0100

    Added address entry dialogs

--- Serwer/MainWindow.xaml.cs
+++ Serwer/MainWindow.xaml.cs
@@ -24,18 +24,15 @@ namespace Serwer
     {
         const string VIDEO_URL = "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov";
         readonly LibVLC _libvlc;
-
         public MainWindow()
         {
             //var databasePath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "VideoSources.db");
-            var databasePath = "D:\\Projects\\Sowa\\Sowa\\Serwer\\VideoSources.db";
-            var db = new SQLiteConnection(databasePath);
+            //var databasePath = "D:\\Projects\\Sowa\\Sowa\\Serwer\\VideoSources.db";
+            //var db = new SQLiteConnection(databasePath);
             InitializeComponent();            // this will load the native libvlc library (if needed, depending on the platform). 
             Core.Initialize();                // instantiate the main libvlc object
             _libvlc = new LibVLC();
-            db.Insert(new VideoSources {Name="test", Address="1.1.1.1" });
-            VideoSources x = db.Table<VideoSources>().First(p => p.Id.Equals(1));
-            x.Name = "chuj";
+            //db.Insert(new VideoSources {Name="test", Address="1.1.1.1" });
             VideoView.MediaPlayer = new LibVLCSharp.Shared.MediaPlayer(_libvlc);
             var rtsp1 = new Media(_libvlc, VIDEO_URL, FromType.FromLocation);       //Create a media object and then set its options to duplicate streams - 1 on display 2 as RTSP
             rtsp1.AddOption(":sout=#duplicate" +
@@ -45,6 +42,11 @@ namespace Serwer
             //VideoView1.MediaPlayer = new LibVLCSharp.Shared.MediaPlayer(_libvlc);
             //VideoView1.MediaPlayer.Mute=true;
             //VideoView1.MediaPlayer.Play(new Media(_libvlc, "rtsp://192.168.0.110:8080/go.sdp", FromType.FromLocation));
+        }
+        private void OnClickSettings(object sender, RoutedEventArgs e)
+        {
+            SettingsWindow settings = new SettingsWindow();
+            settings.Show();
         }
     }
 }
---
+++ Serwer/SettingsWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Serwer
+{
+    /// <summary>
+    /// Interaction logic for SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+
+        public SettingsWindow()
+        {
+            var databasePath = "D:\\Projects\\Sowa\\Sowa\\Serwer\\VideoSources.db";
+            var db = new SQLiteConnection(databasePath);
+            List <VideoSources> videosList = db.Table<VideoSources>().ToList();
+            InitializeComponent();
+            lbVideoSourcesList.ItemsSource = videosList;
+        }
+        private void DeleteFromTable(int id, SQLiteConnection db)
+        {
+            db.Delete<VideoSources>(id);
+        }
+    }
+}
